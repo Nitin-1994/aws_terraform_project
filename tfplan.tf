de3ab5a09a2b7e0577c073f32df1b76d6f78d180
Nitin-1994 <nitinsatti1994@gmail.com>
2025-01-25 17:33:10 +0530

    Update tfplan.tf

--- tfplan.tf
+++ tfplan.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "ourfirst" {
   ami           = "ami-0ac4dfaf1c5c0cce9"
   instance_type = "t2.micro"
   lifecycle {
-    prevent_destroy = true
+    prevent_destroy = false
     ignore_changes  = ["ami", ]
   }
 
